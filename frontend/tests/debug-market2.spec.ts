991e0673a5b0abe3f6ab15a62759304c621e6d1a
Hush <hush@hush7.online>
2026-08-30 15:44:49 +0000

    chore(jobintel-v2): remove temporary debug market specs

--- frontend/tests/debug-market2.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('debug market DOM', async ({ page }) => {
-  await page.goto('/market')
-  await page.waitForLoadState('networkidle')
-  console.log('html', await page.content())
-  console.log('links', await page.locator('a').all())
-  console.log('first a', await page.locator('a').first().getAttribute('href'))
-})
